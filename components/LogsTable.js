0b419afeee88ed0807849d006701be63b0ecbb5a
“SeungKyun <qkrtmdrbs@gmail.com>
2017-03-22 10:29:17 +0900

    1. 로그 출력 순서를 역순으로 변경. 2. 날짜 형식을 간소화된 형태로 변경.

--- components/LogsTable.js
+++ components/LogsTable.js
@@ -4,50 +4,51 @@ import { debounce } from 'lodash'
 import moment from 'moment'
 
 const parseProcess = (str) => {
-  const matches = str.match(/\[(.*)\]/)
-  return matches[1] || str
+    const matches = str.match(/\[(.*)\]/)
+    return matches[1] || str
 }
 
 const parseProcessV2 = (str) => (
-  str
+    str
 )
 
 const parseLine = line => {
-  const firstSpace = line.indexOf(' ')
-  const secondSpace = line.indexOf(' ', firstSpace + 1)
-  const raw = line
-  const date = line.slice(0, firstSpace).trim()
-  const process = parseProcess(line.slice(firstSpace, secondSpace).trim())
-  const text = line.slice(secondSpace, line.length)
+    const firstSpace = line.indexOf(' ')
+    const secondSpace = line.indexOf(' ', firstSpace + 1)
+    const raw = line
+    const date = line.slice(0, firstSpace).trim()
+    const process = parseProcess(line.slice(firstSpace, secondSpace).trim())
+    const text = line.slice(secondSpace, line.length)
 
-  return { date, process, text, raw }
+    return { date, process, text, raw }
 }
 
 const parseLineV2 = line => {
-  const firstSpace = line.indexOf(' ')
-  const raw = line
-  const process = parseProcessV2(line.slice(0, firstSpace).trim())
-  const text = line.slice(firstSpace + 4, line.length)
+    const firstSpace = line.indexOf(' ')
+    const raw = line
+    const process = parseProcessV2(line.slice(0, firstSpace).trim())
+    const text = line.slice(firstSpace + 4, line.length)
 
-  return { process, text, raw }
+    return { process, text, raw }
 }
 
 // date, app, process
 const parseLogs = (logs) => {
-  const splitLogs = logs.split('\n')
-  /* eslint-disable no-console */
-  // detect v2 log format
-  if (splitLogs.length === 1 && logs.substr(0, 2) === 'b\'' && logs[logs.length - 1] === '\'') {
-    console.log('v2 style log')
-    return logs.substr(2, logs.length - 3).split('\\n')
-      .map((line) => line.trim())
-      .filter((line) => line !== '')
-      .map(parseLineV2)
-  }
-  return splitLogs
-    .map((line) => line.trim())
-    .filter((line) => line !== '')
-    .map(parseLine)
+    const splitLogs = logs.split('\n')
+        /* eslint-disable no-console */
+        // detect v2 log format
+    if (splitLogs.length === 1 && logs.substr(0, 2) === 'b\'' && logs[logs.length - 1] === '\'') {
+        console.log('v2 style log')
+        return logs.substr(2, logs.length - 3).split('\\n')
+            .map((line) => line.trim())
+            .filter((line) => line !== '')
+            .map(parseLineV2)
+    }
+    return splitLogs
+        .reverse()
+        .map((line) => line.trim())
+        .filter((line) => line !== '')
+        .map(parseLine)
 }
 
 // Merge all rows together, then extract keys
@@ -57,131 +58,138 @@ const getColumns = (logs) => (
 )
 */
 
-
 export default class LogsTable extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.onFilter = this.onFilter.bind(this)
-    this.togglePrettyDate = this.togglePrettyDate.bind(this)
+        this.onFilter = this.onFilter.bind(this)
+        this.togglePrettyDate = this.togglePrettyDate.bind(this)
 
-    const rawLogs = props.logs
+        const rawLogs = props.logs
 
-    this.state = {
-      logs: parseLogs(rawLogs),
-      filter: {
-        process: '',
-        text: '',
-      },
-      pendingFilter: {
-        process: '',
-        text: '',
-      },
-      prettyDate: false,
-    }
-  }
-
-  onFilter(colName) {
-    const debouncedFn = debounce((value) => {
-      this.setState({
-        filter: {
-          ...this.state.filter,
-          [colName]: value,
-        },
-      })
-    }, 300)
-    return (e) => {
-      // https://github.com/facebook/react/issues/2850
-      this.setState({
-        pendingFilter: {
-          ...this.state.pendingFilter,
-          [colName]: e.target.value,
-        },
-      })
-      debouncedFn(e.target.value)
-    }
-  }
-
-  togglePrettyDate() {
-    this.setState({
-      prettyDate: !this.state.prettyDate,
-    })
-  }
-
-  render() {
-    // TODO: move this to reducer?
-    // const columns = getColumns(logs)
-    const { logs, filter } = this.state
-
-    let filteredLogs = logs
-
-    if (filter.process) {
-      filteredLogs = filteredLogs.filter((row) => (
-        row.process && row.process.indexOf(filter.process) >= 0
-      ))
+        this.state = {
+            logs: parseLogs(rawLogs),
+            filter: {
+                process: '',
+                text: '',
+            },
+            pendingFilter: {
+                process: '',
+                text: '',
+            },
+            prettyDate: true,
+        }
     }
 
-    if (filter.text) {
-      filteredLogs = filteredLogs.filter((row) => (
-        row.text && row.text.indexOf(filter.text) >= 0
-      ))
+    onFilter(colName) {
+        const debouncedFn = debounce((value) => {
+            this.setState({
+                filter: {
+                    ...this.state.filter,
+                    [colName]: value,
+                },
+            })
+        }, 300)
+        return (e) => {
+            // https://github.com/facebook/react/issues/2850
+            this.setState({
+                pendingFilter: {
+                    ...this.state.pendingFilter,
+                    [colName]: e.target.value,
+                },
+            })
+            debouncedFn(e.target.value)
+        }
     }
 
-    const trs = filteredLogs.map((row, idx) => {
-      const date = this.state.prettyDate
-        ? moment.utc(row.date).fromNow()
-        : row.date
-      return (
-        <tr key={idx}>
-          <td className="col-date">{date}</td>
-          <td className="col-process">{row.process}</td>
-          <td className="col-text">{row.text}</td>
-        </tr>
-      )
-    })
+    togglePrettyDate() {
+        this.setState({
+            prettyDate: !this.state.prettyDate,
+        })
+    }
 
-    return (
-      <div className="logs-table">
-        <div className="form">
-          <div className="col-md-4">
-            <input
-              className="form-control"
-              placeholder="Filter by process"
-              value={this.state.pendingFilter.process}
-              onChange={this.onFilter('process')}
-            />
-          </div>
-          <div className="col-md-6">
-            <input
-              className="form-control"
-              placeholder="Search logs"
-              value={this.state.pendingFilter.text}
-              onChange={this.onFilter('text')}
-            />
-          </div>
-          <div className="col-md-2">
-            <Input type="checkbox"
-              label="Pretty dates" checked={this.state.prettyDate}
-              onClick={this.togglePrettyDate}
-              readOnly
-            />
-          </div>
-        </div>
-        <Table striped condensed hover>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th className="col-process">Process</th>
-              <th>Message</th>
-            </tr>
-          </thead>
-          <tbody>
-            {trs}
-          </tbody>
-        </Table>
-      </div>
-    )
-  }
+    render() {
+        // TODO: move this to reducer?
+        // const columns = getColumns(logs)
+        const { logs, filter } = this.state
+
+        let filteredLogs = logs
+
+        if (filter.process) {
+            filteredLogs = filteredLogs.filter((row) => (
+                row.process && row.process.indexOf(filter.process) >= 0
+            ))
+        }
+
+        if (filter.text) {
+            filteredLogs = filteredLogs.filter((row) => (
+                row.text && row.text.indexOf(filter.text) >= 0
+            ))
+        }
+
+        const trs = filteredLogs.map((row, idx) => {
+            const date = this.state.prettyDate ?
+                moment.utc(row.date).fromNow() :
+                row.date
+            return ( <
+                tr key = { idx } >
+                <
+                td className = "col-date" > { date } < /td> <
+                td className = "col-process" > { row.process } < /td> <
+                td className = "col-text" > { row.text } < /td> < /
+                tr >
+            )
+        })
+
+        return ( <
+            div className = "logs-table" >
+            <
+            div className = "form" >
+            <
+            div className = "col-md-4" >
+            <
+            input className = "form-control"
+            placeholder = "Filter by process"
+            value = { this.state.pendingFilter.process }
+            onChange = { this.onFilter('process') }
+            /> < /
+            div > <
+            div className = "col-md-6" >
+            <
+            input className = "form-control"
+            placeholder = "Search logs"
+            value = { this.state.pendingFilter.text }
+            onChange = { this.onFilter('text') }
+            /> < /
+            div > <
+            div className = "col-md-2" >
+            <
+            Input type = "checkbox"
+            label = "Pretty dates"
+            checked = { this.state.prettyDate }
+            onClick = { this.togglePrettyDate }
+            readOnly /
+            >
+            <
+            /div> < /
+            div > <
+            Table striped condensed hover >
+            <
+            thead >
+            <
+            tr >
+            <
+            th > Date < /th> <
+            th className = "col-process" > Process < /th> <
+            th > Message < /th> < /
+            tr > <
+            /thead> <
+            tbody > { trs } <
+            /tbody> < /
+            Table > <
+            /div>
+        )
+    }
 }
 
 
